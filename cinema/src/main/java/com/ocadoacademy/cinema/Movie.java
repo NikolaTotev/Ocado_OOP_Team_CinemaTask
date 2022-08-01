572268d10bde363e8e91446915bc64a43f52ccd6
RalitsaTrendafilova <rally0307@gmail.com>
2022-08-01 14:55:17 +0300

    Changes Movie

--- cinema/src/main/java/com/ocadoacademy/cinema/Movie.java
+++ cinema/src/main/java/com/ocadoacademy/cinema/Movie.java
@@ -8,13 +8,24 @@ public class Movie {
     private final String director;
 
     public Movie(String name, Genre genre, int lengthOfMovie, String actors, String director) throws IllegalArgumentException {
-        assertMovieData(name, genre, lengthOfMovie, actors, director);
+        assertMovieInputData(name, genre, lengthOfMovie, actors, director);
 
         this.name = name;
         this.genre = genre;
         this.lengthOfMovie = lengthOfMovie;
         this.actors = actors;
         this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "Name: " + name + '\'' +
+                ", Genre: " + genre +
+                ", Length of movie: " + lengthOfMovie +
+                ", Actors: " + actors + '\'' +
+                ", Director: " + director + '\'' +
+                '}';
     }
 
     public Genre getGenre() {
@@ -24,23 +35,27 @@ public class Movie {
     public int getLengthOfMovie() {
         return lengthOfMovie;
     }
-    private void assertMovieData(String name, Genre genre, int lengthOfMovie, String actors, String director) throws IllegalArgumentException {
+
+    private void assertMovieInputData(String name, Genre genre, int lengthOfMovie, String actors, String director) throws IllegalArgumentException {
         assertStringData(name);
         assertStringData(actors);
         assertStringData(director);
         assertLengthOfMovie(lengthOfMovie);
     }
-    private void assertStringData(String data) throws IllegalArgumentException{
+
+    private void assertStringData(String data) throws IllegalArgumentException {
         if (data == null) {
             throw new IllegalArgumentException("String data is null");
         }
     }
+
     private void assertLengthOfMovie(int lengthOfMovie) throws IllegalArgumentException {
         if (lengthOfMovie <= 0) {
             throw new IllegalArgumentException("Length of movie must be a positive number");
         }
     }
-    private void assertGenre(Genre genre) throws IllegalArgumentException{
+
+    private void assertGenre(Genre genre) throws IllegalArgumentException {
         if (genre == null) {
             throw new IllegalArgumentException("Invalid genre data");
         }
